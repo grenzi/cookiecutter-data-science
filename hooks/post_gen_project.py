d8e7cf698ab1fd82b9ee16c3fb6f11dfa54f0b6b
gage renzi <gagerenzi@gmail.com>
2017-06-05 21:14:18 -0500

    fix hook, i think

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,4 @@
-""" Hooks to create a new conda environment with the latest version of common libraries 
+""" Hooks to create a new conda environment with the latest version of common libraries
 
 Also optionally create a git repo and commit the project contents
 """
@@ -9,13 +9,12 @@ import shlex
 # The string in these variables will be overriden by cookiecutter
 PROJECT_NAME = "{{cookiecutter.project_name|lower|replace(' ', '_')}}"
 PYTHON_VERSION = "{{cookiecutter.python_version}}"
-PYTHON_LIBRARIES = "{{cookiecutter.python_libraries}}"
 
-# Create the strings for commands to run, substituting with the values from cookiecutter 
-conda_install_cmd_tpl = "conda create -y -n {project_name} python={python_version} {python_libraries}"
+# Create the strings for commands to run, substituting with the values from cookiecutter
+conda_install_cmd_tpl = "conda create -y -n {project_name} python={python_version} "
 conda_install_cmd = conda_install_cmd_tpl.format(project_name=PROJECT_NAME,
                                                  python_version=PYTHON_VERSION,
-                                                 python_libraries=PYTHON_LIBRARIES)
+                                                 python_libraries="./src/requirements.txt")
 conda_save_env_cmd = "conda env export -n {project_name} -f environment.yml".format(project_name=PROJECT_NAME)
 
 # Execute command with subprocess module
