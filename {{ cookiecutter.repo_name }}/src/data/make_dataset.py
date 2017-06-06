b4fb369bd6f9a86529673aafa29412f48c2b1db6
gage renzi <gagerenzi@gmail.com>
2017-06-05 21:57:39 -0500

    alter __file__ in py source

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -12,7 +12,7 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__ or 'make_dataset')
     logger.info('making final data set from raw data')
 
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+    project_dir = os.path.join(os.path.dirname(__file__ or 'make_dataset'), os.pardir, os.pardir)
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
