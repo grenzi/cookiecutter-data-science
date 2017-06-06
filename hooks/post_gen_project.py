37d9508095209e8ba92497ea363b6a9592afdafe
gage renzi <gagerenzi@gmail.com>
2017-06-05 21:18:13 -0500

    final fix hook, i hope

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,12 +9,13 @@ import shlex
 # The string in these variables will be overriden by cookiecutter
 PROJECT_NAME = "{{cookiecutter.project_name|lower|replace(' ', '_')}}"
 PYTHON_VERSION = "{{cookiecutter.python_version}}"
+PYTHON_LIBRARIES = "{{cookiecutter.python_libraries}}"
 
 # Create the strings for commands to run, substituting with the values from cookiecutter
 conda_install_cmd_tpl = "conda create -y -n {project_name} python={python_version} "
 conda_install_cmd = conda_install_cmd_tpl.format(project_name=PROJECT_NAME,
                                                  python_version=PYTHON_VERSION,
-                                                 python_libraries="./src/requirements.txt")
+                                                 python_libraries=PYTHON_LIBRARIES)
 conda_save_env_cmd = "conda env export -n {project_name} -f environment.yml".format(project_name=PROJECT_NAME)
 
 # Execute command with subprocess module
